5ce28c27180e5fac1e93192af4b73f77d514787f
m-okeefe <books.mokeefe@gmail.com>
2018-03-20 14:46:39 -0400

    Linear time, space

--- questions/2-product-all.py
+++ questions/2-product-all.py
@@ -1,29 +1,43 @@
-# interview cake - product of all other numbers
 
-"""
-Given a list of ints, return a list of products of every integer except the integer at that index.
-
-given:   [1, 7, 3, 4]
-return [7*3*4, 1*3*4, 1*7*4, 1*7*3]
-
-constraint: not allowed to use division
-
-"""
-
-def prod_all(A):
+# given list of ints, return list of products of every int except the one at that index
+# first, brute force solution-- O(n^2) time, O(n) space
+def prod_all_brute(A):
     P = []
     for i in range(0, len(A)):
         # calculate product to pivot
-        L,R = 1 # setup
+        L = 1 # setup
+        R = 1
         for x in range(0, i):
             L=L*A[x]
         # calculate product after pivot
-        for y in range(i+1, len(A))
+        for y in range(i+1, len(A)):
             R=R*A[y]
         # append product of L, R products to P
         P.append(L*R)
-    return P
+    print(P)
+
+#prod_all_brute([1,7,3,4])
+#prod_all_brute([1,0,3,4])
 
 
+# now do this in O(n) time, O(n) space
+def prod_all_linear(A):
+    P = [None] * len(A)
 
-print prod_all([1,7,3,4])
+    accum = 1
+    # go forwards (prod_all before index i )
+    for i in range(0, len(A)):
+        P[i] = accum #accum starts at 1 -- QUEUE UP
+        accum *= A[i] #now for the next iteration, accum is the element before me
+
+
+    accum = 1 #reset accum
+    #go backwards (prod_all after index i )
+    for i in range(len(A)-1, -1, -1):
+        P[i] *= accum
+        accum *= A[i]
+
+    print(P)
+
+
+prod_all_linear([1,7,3,4])
