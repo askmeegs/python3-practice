a7a5df1cefa1c52abd54d0f35063497b8a141eab
m-okeefe <books.mokeefe@gmail.com>
2018-03-20 14:28:36 -0400

    bruteforce

--- questions/2-product-all.py
+++ questions/2-product-all.py
@@ -2,13 +2,28 @@
 
 """
 Given a list of ints, return a list of products of every integer except the integer at that index.
+
+given:   [1, 7, 3, 4]
+return [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+
+constraint: not allowed to use division
+
 """
 
-def get_products_of_all_ints_except_at_index(A):
-    P = [prod(1, len(A)-1)]
-    for i in range(1, len(A-1))
-
-
-
-    P = append(P, prod(0,))
+def prod_all(A):
+    P = []
+    for i in range(0, len(A)):
+        # calculate product to pivot
+        L,R = 1 # setup
+        for x in range(0, i):
+            L=L*A[x]
+        # calculate product after pivot
+        for y in range(i+1, len(A))
+            R=R*A[y]
+        # append product of L, R products to P
+        P.append(L*R)
     return P
+
+
+
+print prod_all([1,7,3,4])
