91318c798a38129d9eb3449f0a5e0c1c0804945a
m-okeefe <books.mokeefe@gmail.com>
2018-03-20 13:53:42 -0400

    init

---
+++ questions/2-product-all.py
@@ -0,0 +1,14 @@
+# interview cake - product of all other numbers
+
+"""
+Given a list of ints, return a list of products of every integer except the integer at that index.
+"""
+
+def get_products_of_all_ints_except_at_index(A):
+    P = [prod(1, len(A)-1)]
+    for i in range(1, len(A-1))
+
+
+
+    P = append(P, prod(0,))
+    return P
